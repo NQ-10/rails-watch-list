a82681e71aeb584a8bafdd7ebf70586d8742b583
Nancy Qian <luyao.q@gmail.com>
2023-08-25 00:21:57 +0200

    movielist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+List.destroy_all
+Movie.destroy_all
+
+puts "create movie"
+movie = Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+movie2 = Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+movie3 = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+movie4 = Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+puts "create lists"
+list = List.create(name: "hollywood")
+list2 = List.create(name: "classics")
+
+puts "create bookmarks"
+Bookmark.create(comment: "heard from friend", list_id: list.id, movie_id: movie.id)
+Bookmark.create(comment: "searched from google", list_id: list.id)
+Bookmark.create(comment: "recommend by A", list_id: list2.id)
+Bookmark.create(comment: "recommend by B", list_id: list2.id)
+
+puts "all done"
---
+++ vendor/Todo.md
@@ -0,0 +1,26 @@
+1. Data Bricks
+    - [X] generate empty db
+    - [X] draw db schema
+    - [X] generate model/table movie & list & bookmark
+    - [X] validate model movie & list & bookmark
+    - [X] associate model/table movie & list & bookmark
+    - [X] db:seed movie & list & bookmark
+
+2. Applicative Bricks
+    - [X] generate empty controller list
+
+3. User Action
+    - [X] As a user, I can see all my movie lists
+    - [X] As a user, I can see the details of a movie list
+    - [X] As a user, I can create a movie list
+    - [X] As a user, I can bookmark a movie inside a movie list
+    - [X] As a user, I can destroy a bookmark
+
+4. Silo Actions
+    - [X] GET "lists" - route / controller / view
+    - [X] GET "lists/42"- route / controller / view
+    - [X] GET "lists/new" - route / controller / view
+    - [X] POST "lists" - route / controller / view
+    - [X] GET "lists/42/bookmarks/new" - route / controller / view
+    - [X] POST "lists/42/bookmarks" - route / controller / view
+    - [X] DELETE "bookmarks/25"- route / controller / view
